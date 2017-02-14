de5641ef54f09b92207fce2fc38249d828b4aaa4
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-14 20:19:10 +0530

    Add close button to hide/delete the box

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 Vue.component('message', {
     props: ['title', 'body'],
 
+    data(){
+        return{
+            isVisible: true
+        };
+    },
+
     template: `
     
-        <div class="message">
+        <div class="message" v-show="isVisible">
             <div class="title">
                 <h3>{{title}}</h3>
-                <button class="close">X</button>
+                <button class="close" @click="isVisible = false">X</button>
             </div>
             <div class="body">
                 <p>{{body}}</p>
