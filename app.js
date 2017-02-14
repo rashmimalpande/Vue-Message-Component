274d58c69e079363a592199bf39f3550f003dff0
Rashmi Malpande <rashmimalpande@gmail.com>
2017-02-14 19:47:19 +0530

    initialize a basic component

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+Vue.component('message', {
+    template: `
+    
+        <div class="message">
+            <h1>Hello World</h1>
+        </div>
+    
+    `
+});
+
 var app = new Vue({
     el: '#app',
     data: {
